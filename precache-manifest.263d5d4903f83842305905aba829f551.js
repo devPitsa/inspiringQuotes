411dd1f82bd717a27c8058f5bfa31dfc717c1a45
Vinicius Monteiro Dias <vinicius-mdias@hotmail.com>
2020-01-06 15:17:12 -0300

    Updates

---
+++ precache-manifest.263d5d4903f83842305905aba829f551.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad583ec19aa2f5da2260a791cca801c1",
+    "url": "/inspiringQuotes/index.html"
+  },
+  {
+    "revision": "724ce1fea718a6dfe63d",
+    "url": "/inspiringQuotes/static/css/main.0de62c7f.chunk.css"
+  },
+  {
+    "revision": "e1b17f2a8d08e874d727",
+    "url": "/inspiringQuotes/static/js/2.3827a9aa.chunk.js"
+  },
+  {
+    "revision": "65ac660a5642cbffc3f9ff0a1a2e3ebe",
+    "url": "/inspiringQuotes/static/js/2.3827a9aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "724ce1fea718a6dfe63d",
+    "url": "/inspiringQuotes/static/js/main.c3aa9143.chunk.js"
+  },
+  {
+    "revision": "50676e5abdc5ba21c0de",
+    "url": "/inspiringQuotes/static/js/runtime-main.e6cb866a.js"
+  },
+  {
+    "revision": "6a7f63dafdcd2b1be1821c6847117d9f",
+    "url": "/inspiringQuotes/static/media/background2.6a7f63da.png"
+  }
+]);
